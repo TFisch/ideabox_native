36fe1ee7afc6d3e30d4822caddabfd18ba8d96ea
Tim Fischer <emailtimfischer@gmail.com>
2018-10-10 22:33:18 -0600

    Create header component

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header/Header';
 
 const App = () => (
   <View style={{ flex: 1 }}>
+    <Header headerText={'IdeaBox'} />
   </View>
 );
 
